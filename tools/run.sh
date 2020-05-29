061669cecf1dd47c55dd743cefcf5d393899ecba
Sweta <sweta.vooda@gmail.com>
2020-05-29 21:27:07 +0530

    deleted changes made in run.sh and post1

--- tools/run.sh
+++ tools/run.sh
@@ -17,7 +17,7 @@ WORK_DIR=$(dirname $(dirname $(realpath "$0")))
 CONTAINER=.container
 SYNC_TOOL=_scripts/sh/sync_monitor.sh
 
-cmd="bundle exec jekyll s>>C:\Users\sweta\Documents\output.txt"
+cmd="bundle exec jekyll s"
 realtime=false
 
 
